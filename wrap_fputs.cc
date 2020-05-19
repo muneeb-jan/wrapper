c751fa4d1dc5a647114d26777d10c12fccb6b6f3
Oleksii Oleksenko <alexo_o@ukr.net>
2020-05-19 16:11:51 +0200

    Lab migration

--- wrap_fputs.cc
+++ wrap_fputs.cc
@@ -5,20 +5,18 @@
 
 #include <list>
 
-// obtain list of currenlty opend file handles by wrapping:
+// obtain a list of currently open file handles by wrapping:
 // fopen
 // fdopen
 // freopen
 // fclose
 
 // check for readability of a string (using setjump/longjmp/signal approach)
-static bool check_read_str (const char* s)
-{
+static bool check_read_str(const char *s) {
 }
 
 // check read-/writeability of a file handle (using setjump/longjmp/signal approach)
-static bool check_readwrite_FILE (FILE* f)
-{
+static bool check_readwrite_FILE(FILE *f) {
 }
 
 // WRAP fputs to behave robust
